b9c34f0a886c1791e0b60f4f95f43c5137811c21
Sudheer5V <sudheerbannu9@gmail.com>
2023-06-13 17:34:40 +0530

    ip change

--- src/axios.js
+++ src/axios.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL:
-    "http://ec2-65-0-130-92.ap-south-1.compute.amazonaws.com:8080/v1/app/",
+  baseURL: "http://13.200.64.48:8080/v1/app/",
 });
 
 export default instance;
